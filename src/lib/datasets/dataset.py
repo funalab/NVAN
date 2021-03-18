fixed normalization in dataset.py

src/lib/datasets/dataset.py
@@ -17,6 +17,7 @@ class EmbryoDataset(Dataset):
         with open(os.path.join(self.root, 'labels', 'abort.txt'), 'r') as f:
             self.abort_list = [line.rstrip() for line in f]
         self.criteria_list = csv_loader_criteria_list(os.path.join(self.root, 'input', self.file_list[0], 'criteria.csv'))
+        self.eps = 0.000001
 
     def __len__(self):
         return len(self.file_list)
@@ -35,7 +36,8 @@ class EmbryoDataset(Dataset):
         return label
 
     def normalization(self, vec):
-        return np.array([(v - np.mean(v)) / np.std(v) for v in vec]).astype(np.float32)
+        vec = vec.transpose(1, 0)
+        return np.array([(v - np.mean(v)) / (np.std(v) + self.eps) for v in vec]).transpose(1, 0).astype(np.float32)
 
     def transform(self, vec):
         return np.array(vec[int(random.uniform(0, 20)):-int(random.uniform(0, 20))]).astype(np.float32)