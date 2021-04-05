d5733743fb1bd1ce53eeac429842e048e7719f79
Yuta <tokuoka@fun.bio.keio.ac.jp>
2021-04-06 00:41:21 +0900

    fixed Attention LSTM model for training and inference

--- src/lib/models/LSTMAttention.py
+++ src/lib/models/LSTMAttention.py
@@ -25,20 +25,22 @@ class LSTMAttention(nn.Module):
         self.loss = lossfun
         self.phase = phase
 
-    def attention(self, lstm_out, final_state):
-        hidden = final_state[-1]
-        attn_weights = torch.bmm(lstm_out, hidden.unsqueeze(2)).squeeze(2)
+    def attention(self, lstm_out):
+        batch, time, dim = lstm_out.shape
+        # attn_weights: [batch, time]
+        attn_weights = torch.bmm(lstm_out, lstm_out.permute(0, 2, 1)[:,:,-1].unsqueeze(2)).squeeze(2)
         soft_attn_weights = F.softmax(attn_weights, 1)
+        # new_hidden_state: [batch, dim]
         new_hidden_state = torch.bmm(lstm_out.transpose(1, 2), soft_attn_weights.unsqueeze(2)).squeeze(2)
         return new_hidden_state, soft_attn_weights
 
     def forward(self, input):
-        lstm_out, (final_hidden_state, _) = self.lstm(input.permute(1, 0, 2))
+        lstm_out, _ = self.lstm(input.permute(1, 0, 2))
         lstm_out = lstm_out.permute(1, 0, 2)
-        attn_out, attn_weights = self.attention(lstm_out, final_hidden_state)
+        attn_out, attn_weights = self.attention(lstm_out)
         logits = self.affine(attn_out)
 
         if self.phase == 'test':
-            return logits, attn_weights
+            return logits, None
         else:
             return logits
